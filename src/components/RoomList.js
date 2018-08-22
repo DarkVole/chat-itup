00ccb5d5e1c4b6a35c48a425110f24724ee9c626
wktdev <wktdev@gmail.com>
2018-08-22 11:54:54 -0700

    temp

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,7 +5,6 @@
 
      constructor(props) {
          super(props);
-         this.state = { value: '' };
          this.state = {
              rooms: []
          };
@@ -50,7 +49,7 @@
 
                     <form onSubmit={this.createRoom}>
                        <label>Name:
-                       <input type="text" value={this.state.value} onChange={this.handleChange} />
+                       <input type="text" onChange={this.handleChange} />
                        </label>
                        <input type="submit" value="Submit" />
                     </form>
